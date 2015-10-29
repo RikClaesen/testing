cc2b400b5d1daddf3706bba55a0ad822165b1c77
RikClaesen <rik.claesen@gmail.com>
2015-10-29 16:04:03 +0100

    Test fixed

--- src/test/java/com/example/ResTworkshopApplicationTests.java
+++ src/test/java/com/example/ResTworkshopApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = RestWorkshopApplication.class)
-public class ResTworkshopApplicationTests {
+public class RestWorkshopApplicationTests {
 
 	@Test
 	public void contextLoads() {
